981608e4a47690cc756bc81406fa320e70a55c97
Devashish Upreti <devashup68@gmail.com>
2025-01-30 13:55:36 +0545

    db setup

--- config/database.js
+++ config/database.js
@@ -1,16 +1,16 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
-// const url = "mongodb://localhost:27017/unwindcabins";
-// mongoose.connect(url).then(()=> console.log("DB Connected!"));
+const url = "mongodb://localhost:27017/unwindcabins";
+mongoose.connect(url).then(()=> console.log("DB Connected!"));
 
-mongoose
-  .connect(
-    "mongodb+srv://devashish:devashishUp03@cluster0.gurcn.mongodb.net/",
-    {
-      dbName: "unwindcabins", // Specify the name of the database you want to use
-    }
-  )
-  .then(() => {
-    console.log("DB CONNECTED"); // If the connection is successful
-  })
-  .catch((err) => console.log(err));
+// mongoose
+//   .connect(
+//     "mongodb+srv://devashish:devashishUp03@cluster0.gurcn.mongodb.net/",
+//     {
+//       dbName: "unwindcabins", // Specify the name of the database you want to use
+//     }
+//   )
+//   .then(() => {
+//     console.log("DB CONNECTED"); // If the connection is successful
+//   })
+//   .catch((err) => console.log(err));
